e80010679c54c093d041b71997e66136569eab8e
Nicolas MARTIN <nimartin.pro@gmail.com>
2024-09-05 16:14:14 +0200

    searchbar mobile

--- src/app/modules/layout/components/search-bar/search-bar.component.ts
+++ src/app/modules/layout/components/search-bar/search-bar.component.ts
@@ -18,6 +18,9 @@ export class SearchBarComponent implements OnInit {
   private searchSubject: Subject<string> = new Subject();
   refs: Ref[] = [];  // Stocke les résultats de recherche
   currentSearchText = '';  // Stocke le texte de recherche actuel
+  isDisplayMobileSearch = false;
+  isSearching = false;
+
   constructor(private refService: RefService, private router: Router) { }
 
   ngOnInit(): void {
@@ -29,13 +32,14 @@ export class SearchBarComponent implements OnInit {
   }
 
   onSearchInput(event: any): void {
+    this.isSearching = true;
     const searchText = event.target.value;
     this.searchSubject.next(searchText);
     this.currentSearchText = searchText;  // Met à jour la valeur actuelle
   }
 
   callSearch(searchText: string): void {
-    console.log(searchText);
+
     this.refService.searchRefs(searchText).subscribe({
       next: (refs) => {
         console.log(refs);
@@ -46,8 +50,22 @@ export class SearchBarComponent implements OnInit {
       },
       complete: () => {
         console.log('Search completed');
+        this.isSearching = false;
       }
     });
+  }
+
+  get isIconSearchVisible(): boolean {
+    return (!this.currentSearchText || this.currentSearchText.length === 0);
+  }
+
+  get isIconEmptyVisible(): boolean {
+    return this.currentSearchText?.length > 0  && !this.isSearching && this.refs.length === 0;
+  }
+
+
+  displayMobileSearch(isDisplayMobileSearch : boolean): void {
+    this.isDisplayMobileSearch = isDisplayMobileSearch;
   }
 
   onSearchClick(): void {
@@ -56,5 +74,9 @@ export class SearchBarComponent implements OnInit {
 
   onResultClick(ref: Ref): void {
     this.router.navigate(['/ref', ref.id]);  // Redirige vers une page de détail par exemple
+  }
+
+  redirectToUpload(): void {
+    this.router.navigate(['/ref/upload']);
   }
 }
