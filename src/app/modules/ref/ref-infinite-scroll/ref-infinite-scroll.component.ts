33c524bf72f9d2d7a239945557eed4f2f180c47e
Nicolas MARTIN <nimartin.pro@gmail.com>
2024-09-19 14:11:51 +0200

    change infinite scroll

--- src/app/modules/ref/ref-infinite-scroll/ref-infinite-scroll.component.ts
+++ src/app/modules/ref/ref-infinite-scroll/ref-infinite-scroll.component.ts
@@ -1,7 +1,7 @@
-import { Component, ElementRef, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, Inject, PLATFORM_ID, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { RefService } from '../../../api/ref.service';
 import { Ref } from '../../dashboard/models/ref';
-import { NgFor, NgIf } from '@angular/common';
+import { isPlatformBrowser, NgFor, NgIf } from '@angular/common';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
@@ -13,103 +13,141 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 })
 export class RefInfiniteScrollComponent {
   refs: Ref[] = [];
-  currentIndex = 0;
-  refHeight = 0;
-  hitPerPage = 5;
   page = 0;
+  hitPerPage = 5;
+  currentIndex = 0;
 
-  constructor(private refService: RefService, private elRef: ElementRef, private sanitizer: DomSanitizer) {
-    // Initialize hammerManager in the constructor
+  @ViewChild('scrollContainer', { static: true }) scrollContainer!: ElementRef;
+  @ViewChildren('refItem') refItems!: QueryList<ElementRef>;
+
+  observer!: IntersectionObserver;
+  isBrowser: boolean;
+
+  constructor(
+    private refService: RefService,
+    private sanitizer: DomSanitizer,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
   }
 
   ngOnInit(): void {
     this.loadMoreRefs();
-    this.calculateRefHeight();
   }
 
-  loadMoreRefs(): void {
-    this.refService.searchRefsWithFilters('', '', this.page, this.hitPerPage).subscribe(newRefs => {
-      console.log(newRefs);
-      this.refs = [...this.refs, ...newRefs];
-      // generate tiktokvideo url for each ref
-      this.refs.forEach(ref => {
-        if (!ref.tiktokVideoUrl) {
-          ref.tiktokVideoUrl = this.getTiktokVideoUrl(ref);
-        }
+  ngAfterViewInit() {
+    if (this.isBrowser) {
+      this.initializeObserver();
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
+
+  initializeObserver() {
+    if (typeof window !== 'undefined' && 'IntersectionObserver' in window) {
+      const options = {
+        root: this.scrollContainer.nativeElement,
+        rootMargin: '0px',
+        threshold: 0.5, // 50% visible
+      };
+
+      this.observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          const index = parseInt(entry.target.getAttribute('data-index')!, 10);
+
+          if (entry.isIntersecting) {
+            // La ref est visible à plus de 50%
+            this.pauseTiktok(this.currentIndex);
+            this.currentIndex = index;
+            this.playTiktok(index);
+            this.manageRefLoading();
+          } else {
+            // La ref n'est plus visible
+          }
+        });
+      }, options);
+
+      // Observer chaque refItem
+      this.refItems.forEach((refItem) => {
+        this.observer.observe(refItem.nativeElement);
       });
-    });
+
+      // Observer les changements dans refItems pour les nouvelles refs
+      this.refItems.changes.subscribe((changes: QueryList<ElementRef>) => {
+        changes.forEach((refItem) => {
+          this.observer.observe(refItem.nativeElement);
+        });
+      });
+    } else {
+      // Fallback si IntersectionObserver n'est pas supporté
+      console.warn('IntersectionObserver n\'est pas supporté dans cet environnement.');
+      // Vous pouvez implémenter une alternative ici
+    }
   }
 
   manageRefLoading(): void {
-    if (this.refs.length - this.currentIndex == 2) {
+    if (this.refs.length - this.currentIndex === 2) {
       this.page++;
       this.loadMoreRefs();
     }
   }
 
-  @HostListener('window:resize', [])
-  onResize() {
-    if (typeof window !== "undefined") {
-      this.calculateRefHeight();
-    }
+  loadMoreRefs(): void {
+    this.refService
+      .searchRefsWithFilters('', '', this.page, this.hitPerPage)
+      .subscribe((newRefs) => {
+        newRefs.forEach((ref) => {
+          if (!ref.tiktokVideoUrl) {
+            ref.tiktokVideoUrl = this.getTiktokVideoUrl(ref);
+          }
+        });
+        this.refs = [...this.refs, ...newRefs];
+
+        // Après mise à jour de la vue, réinitialiser l'observateur
+        setTimeout(() => {
+          if (this.observer) {
+            this.observer.disconnect();
+          }
+          if (this.isBrowser) {
+            this.initializeObserver();
+          }
+        }, 0);
+      });
   }
 
-  calculateRefHeight() {
-    // Subtract the navbar height if necessary (e.g., 64px)
-    if (typeof window === "undefined") {
-      return;
-    }
-    const navbarHeight = 64;
-    this.refHeight = window.innerHeight - navbarHeight;
-  }
+  playTiktok(index: number): void {
 
-  onNext(): void {
-    if (this.currentIndex < this.refs.length - 1) {
-      this.pauseTiktok();
-      this.currentIndex++;
-      this.restartTiktok();
-    }
-    this.manageRefLoading();
-  }
-
-  onPrev(): void {
-    if (this.currentIndex > 0) {
-      this.pauseTiktok();
-      this.currentIndex--;
-      this.restartTiktok();
-    }
-  }
-
-
-  pauseTiktok(): void {
-    const iframe: HTMLIFrameElement | null = this.elRef.nativeElement.querySelector('#ref'+this.currentIndex + ' iframe');
+    const iframe: HTMLIFrameElement | null =
+      this.refItems.toArray()[index]?.nativeElement.querySelector('iframe');
     if (iframe && iframe.contentWindow) {
-      iframe.contentWindow.postMessage({ type: 'pause', "x-tiktok-player": true }, '*');
-    }
-  }
-
-  restartTiktok(): void{
-    const iframe: HTMLIFrameElement | null = this.elRef.nativeElement.querySelector('#ref'+this.currentIndex + ' iframe');
-    if (iframe && iframe.contentWindow) {
-
       iframe.contentWindow.postMessage({ type: 'seekTo', value: 0, "x-tiktok-player": true }, '*');
       iframe.contentWindow.postMessage({ type: 'unMute', "x-tiktok-player": true }, '*');
-      iframe.contentWindow.postMessage({ type: 'changeVolume', value: 100, "x-tiktok-player": true }, '*');
       iframe.contentWindow.postMessage({ type: 'play', "x-tiktok-player": true }, '*');
-
     }
   }
 
-  get transform(): string {
-    // if currentIndex = 0, translateY = 0
-    return `translateY(-${this.currentIndex * this.refHeight}px)`;
+  pauseTiktok(index: number): void {
+    const iframe: HTMLIFrameElement | null =
+      this.refItems.toArray()[index]?.nativeElement.querySelector('iframe');
+    if (iframe && iframe.contentWindow) {
+      iframe.contentWindow.postMessage(
+        { type: 'pause', 'x-tiktok-player': true },
+        '*'
+      );
+    }
   }
 
   getTiktokVideoUrl(ref: Ref): SafeUrl {
-    return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.tiktok.com/player/v1/' + ref?.tiktokVideoId + this.params);
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      'https://www.tiktok.com/player/v1/' + ref?.tiktokVideoId + this.params
+    );
   }
 
   get params(): string {
-    return '?autoplay=1&music_info=1&description=1&loop=1&rel=0';
+    return '?autoplay=1&music_info=0&description=0&loop=1&rel=0';
   }
 }
